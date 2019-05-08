423172bcaa81fc7462e67528b2999b29a9856705
Tony Trinh <tony19@gmail.com>
2019-05-08 02:11:58 -0500

    Add demo of problem

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Foo/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
+import Foo from './components/Foo.vue';
 
 export default Vue.extend({
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Foo
   }
 });
 </script>
---
+++ src/components/Foo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="foo">
+    MyFoo {{profilePath}}
+  </div>
+</template>
+
+<script lang='ts'>
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        data: function() {
+            return {
+                open: false
+            }
+        },
+        computed: {
+            profilePath: function(): string {
+                return "/user/" + this.$store.state.profile.profile.user.id
+            }
+        },
+        methods: {
+            toggle: function () {
+                this.open = !this.open
+
+                if (this.open) {
+                    // Add click listener to whole page to close dropdown
+                    document.addEventListener('click', this.close)
+                }
+            },
+            close: function () {
+                this.open = false;
+                document.removeEventListener('click', this.close)
+            }
+        }
+    })
+</script>
